5e890aca40b88315dd1e6b655f433e82b2b9fc98
James <TJVvmnsNzH6{Ptbin6PDdDGi>
2020-07-01 18:03:36 +0100

    Remove App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import reactLogo from './Assets/react-logo.png';
 import PostList from './Components/PostList';
 import PostProvider, {PostContext} from './Contexts/PostProvider';
@@ -7,37 +6,35 @@ import UserSelectDropdown from './Components/UserSelectDropdown';
 import PostListItem from './Components/PostListItem';
 import Spinner from './Components/Spinner';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row mt-5">
-        <div className="col-12 d-flex justify-content-center">
-          <img src={reactLogo} alt="React Logo" style={{ width: '200px' }} />
-        </div>
-        <div className="col-12">
-          <PostProvider>
-            <PostContext.Consumer>
-              {({ isLoading, posts, getPostsForUserWithId, deletePostWithId }) => (
-                <>
-                  <UserSelectDropdown handleChange={userId => getPostsForUserWithId(userId)} />
-                  {isLoading ? <Spinner /> : (
-                    <PostList>
-                      {posts.map(post => (
-                        <PostListItem
-                          {...post}
-                          handleDeleteClicked={deletePostWithId}
-                        />
-                      ))}
-                    </PostList>
-                  )}
-                </>
-              )}
-            </PostContext.Consumer>
-          </PostProvider>
-        </div>
+const App = () => (
+  <div className="container">
+    <div className="row mt-5">
+      <div className="col-12 d-flex justify-content-center">
+        <img src={reactLogo} alt="React Logo" style={{ width: '200px' }} />
+      </div>
+      <div className="col-12">
+        <PostProvider>
+          <PostContext.Consumer>
+            {({ isLoading, posts, getPostsForUserWithId, deletePostWithId }) => (
+              <>
+                <UserSelectDropdown handleChange={userId => getPostsForUserWithId(userId)} />
+                {isLoading ? <Spinner /> : (
+                  <PostList>
+                    {posts.map(post => (
+                      <PostListItem
+                        {...post}
+                        handleDeleteClicked={deletePostWithId}
+                      />
+                    ))}
+                  </PostList>
+                )}
+              </>
+            )}
+          </PostContext.Consumer>
+        </PostProvider>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
